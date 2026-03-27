58cf5a522011ae091c1cde69a6efa4119cc9af20
gwenael9 <gwenaelgueho@gmail.com>
2026-03-27 10:35:39 +0100

    init reco

---
+++ mfe-reco/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ background: '#0f0f1a', minHeight: '100vh', padding: '24px' }}>
+      <h2 style={{ color: '#6c5ce7', marginBottom: '16px', textAlign: 'center' }}>
+        Reco
+      </h2>
+      <Reco />
+    </div>
+  );
+}
+
+export default App;
---
+++ mfe-reco/src/components/Reco.jsx
@@ -0,0 +1,9 @@
+function Reco() {
+  return (
+    <div>
+      <h2>Hello</h2>
+    </div>
+  );
+}
+
+export default Reco;
